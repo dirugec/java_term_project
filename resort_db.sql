091fa85823f4e933500789b65e34056c702445bd
Dumar Ruge <dirugec@gmail.com>
2024-02-27 19:03:17 -0500

    new mysql script for creating db

--- resort_db.sql
+++ resort_db.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `RESORT_DB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `RESORT_DB`;
 -- MySQL dump 10.13  Distrib 8.0.25, for macos11 (x86_64)
 --
--- Host: localhost    Database: RESORT_DB
+-- Host: localhost    Database: sojurn_db
 -- ------------------------------------------------------
 -- Server version	8.0.33
 
@@ -18,12 +16,171 @@ USE `RESORT_DB`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping events for database 'RESORT_DB'
+-- Table structure for table `customers`
 --
 
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` int NOT NULL,
+  `balance` decimal(12,2) NOT NULL,
+  `parent_id` int DEFAULT NULL,
+  `password` varchar(45) NOT NULL,
+  `user_type` int NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 --
--- Dumping routines for database 'RESORT_DB'
+-- Dumping data for table `customers`
 --
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (9,'John','Doe','john-doe@gamil.com',123123337,0.00,2,'WhoAmI',1,1);
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detail_trans`
+--
+
+DROP TABLE IF EXISTS `detail_trans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detail_trans` (
+  `det_trans_id` int NOT NULL,
+  `trans_id` int NOT NULL,
+  `date_trans` datetime NOT NULL,
+  `product_id` int NOT NULL,
+  `price` decimal(12,2) NOT NULL,
+  `quantity` decimal(12,2) NOT NULL,
+  PRIMARY KEY (`det_trans_id`),
+  KEY `FK_transaction_det_idx` (`trans_id`),
+  CONSTRAINT `FK_transaction_det` FOREIGN KEY (`trans_id`) REFERENCES `transactions` (`trans_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_trans`
+--
+
+LOCK TABLES `detail_trans` WRITE;
+/*!40000 ALTER TABLE `detail_trans` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_trans` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `merchant`
+--
+
+DROP TABLE IF EXISTS `merchant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `merchant` (
+  `merchant_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`merchant_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `merchant`
+--
+
+LOCK TABLES `merchant` WRITE;
+/*!40000 ALTER TABLE `merchant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `merchant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `price` decimal(12,2) NOT NULL,
+  `merchant_id` int NOT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `FK_merchant_id_idx` (`merchant_id`),
+  CONSTRAINT `FK_merchant_product` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`merchant_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_users`
+--
+
+DROP TABLE IF EXISTS `staff_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff_users` (
+  `staff_id` int NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_users`
+--
+
+LOCK TABLES `staff_users` WRITE;
+/*!40000 ALTER TABLE `staff_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `trans_id` int NOT NULL,
+  `customer_id` int NOT NULL,
+  `date` datetime NOT NULL,
+  `type` int NOT NULL,
+  `amount` decimal(12,2) NOT NULL,
+  `merchant_id` int DEFAULT NULL,
+  PRIMARY KEY (`trans_id`),
+  KEY `FK_customer_id_idx` (`customer_id`),
+  KEY `FK_merchant_id_idx` (`merchant_id`),
+  CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_merchant_id` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`merchant_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -34,4 +191,4 @@ USE `RESORT_DB`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-27 17:52:13
+-- Dump completed on 2024-02-27 19:01:46
